45bf9d2cc0388f16689f2031051441160b464592
Schifrin-Beckham-192110696 <113084063+Schifrin-Beckham-192110696@users.noreply.github.com>
2023-08-29 09:01:48 +0530

    Update 08.c

--- 08.c
+++ 08.c
@@ -5,26 +5,34 @@ int current_state = 0;
 int simulate_NFA(char* input_string) {
     int length = strlen(input_string);
     
-	for (int i = 0; i < length; i++) {
-        if (current_state == 0 && input_string[i] == 'b') {
-            current_state = 1;
-        } else if (current_state == 1 && input_string[i] == 'a') {
-            current_state = 2;
+	for(int i=0;i<length;i++) 
+	{
+        if(current_state==0&&input_string[i]=='b') 
+	{
+            current_state=1;
+        } 
+	else if(current_state==1&&input_string[i]=='a') 
+	{
+            current_state=2;
             return 1;
-        } else {
-            current_state = 0;
+        } 
+	else 
+	{
+            current_state=0;
             return 0;
         }
     }
-    return 0;
 }
 int main() {
     char input_string[MAX_STRING_LENGTH];
     printf("Enter a string to check: ");
-    scanf("%s", input_string);
-    if (simulate_NFA(input_string)) {
-        printf("Accepted\n");
-    } else {
-        printf("Rejected\n");
+    scanf("%s",input_string);
+    if(simulate_NFA(input_string)) 
+    {
+        printf("Accepted");
+    }
+    else
+    {
+        printf("Rejected");
     }
 }
